967f7fdecc3fbdb3c29fc95d46d5a8bf29f109a4
Eyoel Mekonnen <eyoelmoges20@gmail.com>
2026-09-03 23:41:09 +0100

    feat: Create metadata module to reflect tables
    This module contains a function load_tables that reflects
    the tables schema in the connected database

---
+++ metadata.py
@@ -0,0 +1,12 @@
+from sqlalchemy import MetaData
+from database import engine
+
+# Initialise Metadata object
+metadata_obj = MetaData()
+
+# Load table schema to MetaData from pre-exisiting database
+async def load_tables():
+    async with engine.connect() as conn:
+        # Run reflection code synchronusly
+        # Reflection can't be run asynchronusly
+        await conn.run_sync(metadata_obj.reflect)
